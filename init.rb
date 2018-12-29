5c81e2c6231e0233b657a46c0aa0f011a8898f5d
Amir Modarresi <amir.modarresi.13@gmail.com>
2018-12-29 21:52:26 +0330

    update

--- init.rb
+++ init.rb
@@ -6,4 +6,11 @@ Redmine::Plugin.register :sarmad_messaging do
   version '0.0.1'
   url 'http://example.com/path/to/plugin'
   author_url 'http://example.com/about'
+
+  Redmine::MenuManager.map :account_menu do |menu|
+    menu.push(:messages, { controller: 'mailboxer_messages', action: 'index' },
+              param: :messages,
+              caption: :messages,
+              if: proc { !User.current.is_a?(AnonymousUser) })
+  end
 end
