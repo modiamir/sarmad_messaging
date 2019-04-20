01f7e3542cb7d8d3a8c466a65b4e03357aa91cd4
Amir Modarresi <amir.modarresi.13@gmail.com>
2019-04-20 06:32:16 +0430

    add read/unread functionality to mesages

--- app/controllers/mailboxer_messages_controller.rb
+++ app/controllers/mailboxer_messages_controller.rb
@@ -1,9 +1,10 @@
 class MailboxerMessagesController  < ApplicationController
-  before_action :authorize, :except => [:index]
+  before_action :authorize, :except => [:index, :update]
 
   def index
     scope = User.current.mailbox.inbox
 
+    @unread_messages_count = scope.unread(User.first).count
     @messages_count = scope.count
     @messages_pages = Paginator.new @messages_count, 15, (params[:page] || 1)
     @offset ||= @messages_pages.offset
@@ -13,5 +14,11 @@ class MailboxerMessagesController  < ApplicationController
       limit(15).
       offset(@offset).
       to_a
+  end
+
+  def update
+    message = User.current.mailbox.inbox.find(params[:id])
+    message.mark_as_read(User.current)
+    render json: {message: message}
   end
 end
